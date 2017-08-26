cbe19ee08e7168d38aaa99f3f39923443618ea79
JuanDMeGon <juandmegon@gmail.com>
2017-08-26 16:18:57 -0500

    Tu Primer Programa Java: "Hola Mundo"

--- MiPrimerProyecto/src/MiPrimeraClase.java
+++ MiPrimerProyecto/src/MiPrimeraClase.java
@@ -10,8 +10,8 @@
  */
 public class MiPrimeraClase {
     
-    public static void main(String[] args) {
-        
+    public static void main(String args[]) {
+        System.out.println("Hola Mundo");
     }
     
 }
