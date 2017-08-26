4b1238c4b2bcf1ab48d9be9d7fc5d41b3c2f9fea
JuanDMeGon <juandmegon@gmail.com>
2017-08-26 18:56:13 -0500

    Comentarios de Documentación en Java

--- ComentariosJava/src/Comentarios.java
+++ ComentariosJava/src/Comentarios.java
@@ -1,15 +1,15 @@
 public class Comentarios
 {
     
-    /*Este es el método principal
-    df
-    sdf
-    sdf
-    sdf
-    sdf
-    sdf
-    
-    */
+    /**
+     * Este es el método principal
+     * 
+     * 
+     * 
+     * @param args argumentos de consola del método main
+     *
+     *     
+     */
     public static void main(String args[])
     {
         System.out.println("Hola");
