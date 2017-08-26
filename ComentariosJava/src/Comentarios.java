a19d29aeddffd3194dd5b0c38fc136455d982f52
JuanDMeGon <juandmegon@gmail.com>
2017-08-26 18:40:16 -0500

    Comentarios de una Línea en Java

---
+++ ComentariosJava/src/Comentarios.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class Comentarios {
+    //System.out.println();
+    //System.out.println();
+    
+}
