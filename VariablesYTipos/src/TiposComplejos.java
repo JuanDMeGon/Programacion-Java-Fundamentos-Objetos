53d70e1be1637f83a1b09db99cf0cbf94d6cb664
JuanDMeGon <juandmegon@gmail.com>
2017-08-27 18:34:05 -0500

    Tipos Complejos en Java

---
+++ VariablesYTipos/src/TiposComplejos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class TiposComplejos {
+    /*
+    Byte -128 a 127
+    Short -32000 a 32000
+    Integer 20000000000
+    Long grande
+    Float enorme 
+    Double gigante
+    Character 'g'
+    Boolean true o false
+    */
+    
+    public static void main(String args[])
+    {
+        //System.out
+    }
+}
