0d2e3f6469ddd77224f94b811eeadb8c3fd7a1a4
JuanDMeGon <juandmegon@gmail.com>
2017-08-27 18:33:43 -0500

    Tipos de Dato Primitivos en Java

---
+++ VariablesYTipos/src/TiposPrimitivos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class TiposPrimitivos {
+    /*
+    byte -128 a 127
+    short -32000 a 32000
+    int 20000000000
+    long grande
+    float enorme 
+    double gigante
+    char 'g'
+    boolean true o false
+    */
+}
