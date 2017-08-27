670049a722f2397556a68126d5939dcb973059a5
JuanDMeGon <juandmegon@gmail.com>
2017-08-27 18:58:52 -0500

    Cadenas de Caracteres (String) en Java

---
+++ VariablesYTipos/src/CadenasDeCaracteres.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class CadenasDeCaracteres {
+    public static void main(String args[])
+    {
+        System.out.println("ef'e'f''");
+    }
+}
