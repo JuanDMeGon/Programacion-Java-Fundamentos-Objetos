f8dff692c6190ab125b244ee106f7f38ba3804e5
JuanDMeGon <juandmegon@gmail.com>
2017-08-26 19:02:46 -0500

    La Importancia de los Comentarios en Java y la Programación

--- ComentariosJava/src/Comentarios.java
+++ ComentariosJava/src/Comentarios.java
@@ -3,8 +3,7 @@ public class Comentarios
     
     /**
      * Este es el método principal
-     * 
-     * 
+     * Acá es donde comienza todo el proceso de ejecución
      * 
      * @param args argumentos de consola del método main
      *
