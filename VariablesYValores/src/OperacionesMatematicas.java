dae4b64f0050e006f7c03d1b7c066c91480eb6e8
JuanDMeGon <juandmegon@gmail.com>
2017-08-28 17:22:55 -0500

    Operaciones Matemáticas en Java

---
+++ VariablesYValores/src/OperacionesMatematicas.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+
+// Sumas +
+// Restas -
+// Multiplicaciones *
+// Divisiones /
+// Módulo %
+// Agregar ++
+// Sustraer --
+
+public class OperacionesMatematicas {
+    public static void main(String args[])
+    {
+        double primerNumero = 5;
+        double segundoNumero = 2;
+        double resultado;
+        
+        resultado = primerNumero / segundoNumero;
+        
+        System.out.println(resultado);
+        
+    }
+}
