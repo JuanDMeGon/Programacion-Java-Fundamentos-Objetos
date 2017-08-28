48edb1315f374c5474bca29bdc5a05d2085689f8
JuanDMeGon <juandmegon@gmail.com>
2017-08-28 16:42:36 -0500

    Inicialización de Variables en Java

---
+++ VariablesYValores/src/InicializacionDeVariables.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class InicializacionDeVariables {
+    public static void main(String args[])
+    {
+        String cadena = "hola";
+        
+        System.out.println(cadena);
+    }
+}
