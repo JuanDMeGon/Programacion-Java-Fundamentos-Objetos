14738dcc20305ac5490228957aab2b94261bf09e
JuanDMeGon <juandmegon@gmail.com>
2017-08-28 17:38:36 -0500

    Concatenación de Texto y Valores en Java

---
+++ VariablesYValores/src/ConcatenacionDeTexto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class ConcatenacionDeTexto {
+    public static void main(String args[]) {
+        String primerTexto = "Hola";
+        String segundoTexto = "Bienvenido";
+        
+        String textoCompleto = primerTexto + " " + segundoTexto + " ";
+        
+        int numero = 5;
+        int nuevoNumero = 10;
+        
+        System.out.println("" + numero + nuevoNumero);
+    }
+}
