a515275f692475b00b8a03789cdbb1353377b3c1
JuanDMeGon <juandmegon@gmail.com>
2017-08-28 16:33:34 -0500

    Asignación de Valores en Java

---
+++ VariablesYValores/src/AsignacionDeValores.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class AsignacionDeValores {
+    public static void main(String args[])
+    {
+        String cadena;
+        String nuevaCadena;
+        
+        //identificador = valor;
+        
+        cadena = "Hola";
+        nuevaCadena = "Nuevo valor";
+        
+        System.out.println(nuevaCadena);
+        
+        int entero;
+        float flotante;
+        
+        entero = 2;
+        flotante = 5.5f;
+        flotante = entero;
+        
+        System.out.println(entero);
+    }
+}
