a0e4ed8de87a5f33a88b6623694ab7f646f703f7
JuanDMeGon <juandmegon@gmail.com>
2017-08-28 16:20:25 -0500

    Creación de Variables en Java

---
+++ VariablesYValores/src/CreacionDeVariables.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class CreacionDeVariables {
+    public static void main(String args[])
+    {
+        //Tipo nombre;
+        String cadenaDeTexto;
+        int numeroEntero;
+        boolean bandera;
+    }
+}
