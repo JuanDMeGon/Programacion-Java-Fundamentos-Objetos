517e1c79652e14a94d0a156bf2ab6eb4c07f1e1c
JuanDMeGon <juandmegon@gmail.com>
2017-08-27 19:21:18 -0500

    Arreglos y Matrices en Java

---
+++ VariablesYTipos/src/ArreglosYMatrices.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class ArreglosYMatrices {
+    public static void main(String args[])
+    {
+        String x[][] =  {{"1ret", "sdsd"},
+                        {"ert2", "sdsd"},
+                        {"df3", "sdsd"},
+                        {"sdf4", "sdsd"},
+                        {"sdf5", "sdsd"}};
+        System.out.println(x[4][0]);
+        
+    }
+}
