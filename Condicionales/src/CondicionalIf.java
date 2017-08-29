236155545132a63f9463ce91e4215077b5162ec0
JuanDMeGon <juandmegon@gmail.com>
2017-08-29 10:45:27 -0500

    El Condicional "if" en Java

---
+++ Condicionales/src/CondicionalIf.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class CondicionalIf {
+    public static void main(String args[]) {
+        int edad = 17;
+        
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad");          
+        }
+        
+        if (edad < 18) {
+            System.out.println("Eres menor de edad");          
+        }
+        
+        
+    }
+}
