998ae32a2ec12c77ea29ee60bd61324cf9c47417
JuanDMeGon <juandmegon@gmail.com>
2017-09-05 15:15:01 -0500

    Condicionales Anidados en Java

---
+++ Condicionales/src/CondicionalesAnidados.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class CondicionalesAnidados {
+    public static void main(String args[]) {
+        if (true)
+        {
+            if (true)
+            {
+                if(true)
+                {
+                    if (true) {
+                        String x;
+                    }
+                }
+            }
+            
+        }
+    }
+}
