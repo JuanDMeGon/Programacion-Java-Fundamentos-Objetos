2482f0242e52850e55e3b0050cb852ccec7aec15
JuanDMeGon <juandmegon@gmail.com>
2017-09-07 16:10:06 -0500

    El Ciclo "do-while" en Java

---
+++ Ciclos/src/CicloDoWhile.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class CicloDoWhile {
+    public static void main(String[] args) {
+        
+        int opcion;
+        do
+        {
+            System.out.println("Elija una opción");
+            opcion = 10;
+            System.out.println("Eligió la Opción " + opcion);
+        }while(opcion != 10);
+        
+        System.out.println("Finalizó el programa");
+    }
+}
