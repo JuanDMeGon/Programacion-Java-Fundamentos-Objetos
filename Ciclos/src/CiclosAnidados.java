7943196455ca7bbdb3bbb7d11666b507be336d5b
JuanDMeGon <juandmegon@gmail.com>
2017-09-07 16:18:35 -0500

    Ciclos Anidados en Java

---
+++ Ciclos/src/CiclosAnidados.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class CiclosAnidados {
+    public static void main(String[] args) {
+        
+        for(int i = 0; i <= 10; i++)
+        {
+            for(int j = 0; j <= 10; j++)
+            {
+                if(i > 5)
+                {
+                    
+                }
+                
+                while(j > 0)
+                {
+                    
+                }
+                
+                for(int k = 0; k <= 10; k++)
+                {
+                    while(i > 0 && j != 10)
+                    {
+                        do
+                        {
+                            
+                        }
+                        while(true);
+                    }
+                }
+                
+                for(int l = 0; l <= 10; l++)
+                {
+                    
+                }
+            }
+        }
+        
+    }
+}
