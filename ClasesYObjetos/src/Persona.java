71da53513b0d50427f814e5f6644d799ea0ee665
JuanDMeGon <juandmegon@gmail.com>
2017-09-15 18:29:04 -0500

    Los Atributos de una Clase en Java

---
+++ ClasesYObjetos/src/Persona.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class Persona
+{
+    String nombre;
+    int edad;
+    String documento;
+    double estatura;
+    String nacionalidad;
+}
