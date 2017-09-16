c1ce2c0c341b4b858b3ab47c3bf0bb910ba3b0d2
JuanDMeGon <juandmegon@gmail.com>
2017-09-15 19:25:57 -0500

    Creación de Objetos en Java

--- ClasesYObjetos/src/Animal.java
+++ ClasesYObjetos/src/Animal.java
@@ -16,7 +16,18 @@ public class Animal {
     Animal(String nombreDelAnimal, float pesoDelAnimal, String razaDelAnimal)
     {
         nombre = nombreDelAnimal;
-        peso = pesoDelAnimal;
+        peso = pesoDelAnimal + 10;
         raza = razaDelAnimal;
+    }
+    
+    public static void main(String[] args) {
+        Animal animal1;
+        
+        animal1 = new Animal("Juan", 1.5f, "bulldog");
+        
+        Animal animal2 = new Animal("Pepe", 2, "bulldog");
+        
+        System.out.println(animal1.peso);
+        System.out.println(animal2.peso);
     }
 }
