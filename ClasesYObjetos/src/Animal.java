6bfe30439146280d943d49984ac813fe6849cd0b
JuanDMeGon <juandmegon@gmail.com>
2017-09-15 19:12:00 -0500

    Los Constructores de una Clase en Java

---
+++ ClasesYObjetos/src/Animal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class Animal {
+    String nombre;
+    float peso;
+    String raza;
+    
+    Animal(String nombreDelAnimal, float pesoDelAnimal, String razaDelAnimal)
+    {
+        nombre = nombreDelAnimal;
+        peso = pesoDelAnimal;
+        raza = razaDelAnimal;
+    }
+}
