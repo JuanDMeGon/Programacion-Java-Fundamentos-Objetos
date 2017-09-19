d791474bfb4164f9d0f4d092c7d321240593816b
JuanDMeGon <juandmegon@gmail.com>
2017-09-19 16:26:46 -0500

    Atributos Estáticos “static” en Java

--- Aeronautica/src/aeronautica/aeropuertos/Aeropuerto.java
+++ Aeronautica/src/aeronautica/aeropuertos/Aeropuerto.java
@@ -13,10 +13,12 @@ import aeronautica.aviones.Avion;
  */
 public class Aeropuerto {
     Avion[] aviones;
+    static int cantAviones;
     
     Aeropuerto(Avion[] aviones)
     {
         this.aviones = aviones;
+        cantAviones = aviones.length;
     }
     
     Aeropuerto(Avion avion)
@@ -24,6 +26,7 @@ public class Aeropuerto {
         this.aviones = new Avion[50];
         
         this.aviones[0] = avion;
+        cantAviones = 1;
     }
     
     Aeropuerto(Avion avion, int cantidad)
@@ -31,15 +34,28 @@ public class Aeropuerto {
         this.aviones = new Avion[cantidad];
         
         this.aviones[0] = avion;
+        cantAviones = 1;
     }
     
     Aeropuerto(int cantidad)
     {
         this.aviones = new Avion[cantidad];
+        cantAviones = 0;
     }
     
     final Avion[] agregarAvion(Avion avion)
     {
         return this.aviones;
+    }
+    
+    public static void main(String[] args) {
+        Avion avion1 = new Avion(35000, 50000, 0, "i15f52gr");
+        Aeropuerto b = new Aeropuerto(avion1, 10);
+        
+        Aeropuerto a = new Aeropuerto(10);
+        
+        System.out.println(cantAviones);
+        
+        
     }
 }
