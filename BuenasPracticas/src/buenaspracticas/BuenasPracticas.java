aa75b4306e748335a4a0058ada44ec4844851a3b
JuanDMeGon <juandmegon@gmail.com>
2017-09-20 16:47:23 -0500

    Buenas Prácticas de Programación en Java

---
+++ BuenasPracticas/src/buenaspracticas/BuenasPracticas.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buenaspracticas;
+
+/**
+ *
+ * @author JuanDMeGon
+ */
+public class BuenasPracticas {
+
+    private String nombreDelAtributo;
+    int edad;
+    String nombre;
+    private final int VALOR_CONSTANTE = 5;
+    
+    public BuenasPracticas()
+    {
+        
+    }
+    
+    int ejecutar()
+    {
+        return 0;
+    }
+    
+    public int nombreDelMetodo()
+    {
+        otroMetodo(1, "texto");
+        return 0;
+    }
+    
+    public void otroMetodo(int parametro, String otroParametro)
+    {
+        
+    }
+    
+}
